1e4794bc23eff080a33c6f4f806697b6bf967940
Wihann538 <wihannjacobs@gmail.com>
2021-04-04 11:40:44 +0200

    Added comments

--- Main.js
+++ Main.js
@@ -1,15 +1,18 @@
+//Input phrases below are used to create random sentence
 const inputPhrases = {
     Name: ['Wihann', 'Maaijke', 'Ane', 'Christiaan'],
     Verb: ['swims', 'jogs', 'sleeps', 'rows'],
     Place: ['in the dam', 'in the park', 'in the bedroom', 'on the ocean']
 };
 
+// getPhrase function below selects random element from array
 const getPhrase = arr => {
   const arrLen =   arr.length;
   const rand = Math.floor(Math.random()* arrLen);
   return arr[rand];
 };
 
+//generateSentence function below selects a random name, verb and place and creates a sentence
 const generateSentence = phrases => {
     const outputArr = [];
     outputArr.push(getPhrase(phrases.Name));
@@ -20,5 +23,5 @@ const generateSentence = phrases => {
 }
 
 
-//console.log(getPhrase(phrases.Name));
+//Code below prints sentence
 console.log(generateSentence(inputPhrases));
